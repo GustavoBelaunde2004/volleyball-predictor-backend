b5674605556cf6bface227cb20e784b52b06858c
gustavo <gbelaund@trinity.edu>
2024-11-02 21:44:53 -0500

    working w frontend, CORS implementation

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.api.endpoints import predict_set_score, predict_match_details 
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Volleyball Match Predictor API")
+
+# Allow CORS requests
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or specify your frontend URL, e.g., ["http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 # Include the endpoints
 app.include_router(predict_set_score.router, prefix="/predict")
